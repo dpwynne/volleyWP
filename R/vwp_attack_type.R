2a098375abf2ebc55df970f4cb2b5dd1ac1613ff
dpwynne <dpwynne@gmail.com>
2021-07-01 15:31:09 -0700

    document attack type

--- R/vwp_attack_type.R
+++ R/vwp_attack_type.R
@@ -1,3 +1,21 @@
+#' Assign attack type
+#'
+#' Assigns a code to attacks, blocks and digs to indicate the quality of the attack. Currently just recodes hard spikes vs. everything else is off-speed.
+#'
+#' @param plays a dv_plays object or data frame containing play-by-play data
+#'
+#' @return The same data frame, with an "attack_type" variable indicating whether the attack was a hard spike or offspeed attack (tip, roll shot, etc.).
+#'
+#' @importFrom magrittr %>%
+#' @importFrom dplyr mutate
+#' @importFrom dplyr case_when
+#' @importFrom dplyr lag
+#' @importFrom dplyr select
+#' @importFrom rlang .data
+#'
+#' @export
+
+
 vwp_attack_type <- function(plays){
   return(
     plays %>% mutate(attack_type_original =
@@ -10,8 +28,8 @@ vwp_attack_type <- function(plays){
                      attack_type = 
                        case_when(
                          attack_type_original == "Hard spike" ~ attack_type_original,
-                         !is.na(attack_type_original) & attack_type_original != "Hard spike" ~ "Off speed", # if there is an attack type that isn't hard spike, it's off speed
-                         TRUE ~ NA_character_  
+                         !is.na(attack_type_original) & attack_type_original != "Hard spike" ~ "Offspeed", # if there is an attack type that isn't hard spike, it's off speed
+                         TRUE ~ "Unknown"  
                        )  # end 2nd case_when
     ) %>%   # end mutate
       select(-attack_type_original)  # if there is a different in roll shot vs. soft spike, we can remove the second case_when and the select
