3306d4f38a5bdb178844e656d1c43b49012a07e9
dpwynne <dpwynne@gmail.com>
2021-07-01 20:29:28 -0700

    fix quotation marks

--- R/vwp_extract_relevant_features.R
+++ R/vwp_extract_relevant_features.R
@@ -14,11 +14,11 @@
 #' @export
 
 
-vwp_extract_relevant_features <- function(plays, features = c("team, opponent, match_id, point_id, video_time, player_name,
-                                                             skill, skill_type, evaluation_code, evaluation, attack_code, set_code,
-                                                             start_zone, end_zone, end_subzone, skill_subtype, num_players,
-                                                             home_p2, home_p3, home_p4, visiting_p2, visiting_p3, visiting_p4,
-                                                             home_team, visiting_team, point_won_by, serving_team, phase")){
+vwp_extract_relevant_features <- function(plays, features = c("team", "opponent", "match_id", "point_id", "video_time", "player_name",
+                                                             "skill", "skill_type", "evaluation_code", "evaluation", "attack_code", "set_code",
+                                                             "start_zone", "end_zone", "end_subzone", "skill_subtype", "num_players",
+                                                             "home_p2", "home_p3", "home_p4", "visiting_p2", "visiting_p3", "visiting_p4",
+                                                             "home_team", "visiting_team", "point_won_by", "serving_team", "phase")){
   suppressWarnings( # if something is not present in plays then we will get a warning, this suppresses the warning
     plays %>%
     select(one_of(features)) %>%
