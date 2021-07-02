077507b8f0226828286216b9c69369c5eb46ca02
dpwynne <dpwynne@gmail.com>
2021-07-01 19:08:13 -0700

    comment out column selections, use front-end feature selection to do this

--- R/vwp_compute_pwp_workflow.R
+++ R/vwp_compute_pwp_workflow.R
@@ -87,7 +87,7 @@ vwp_compute_pwp <- function(plays, PWP, coef_matrices){
   
   # Step 1: set up all the features we need
   vbID <- plays %>%
-    filter(skill %in% c("Serve", "Reception", "Set", "Attack", "Block", "Dig", "Freeball"))  # get rid of substitutions, timeouts, and NA skills
+    filter(skill %in% c("Serve", "Reception", "Set", "Attack", "Block", "Dig", "Freeball")) %>%  # get rid of substitutions, timeouts, and NA skills
     mutate(id = seq(1, nrow(plays))) %>%  # add an ID column - very important for splitting and recombining 
     vwp_add_cover() %>% # add cover
     vwp_kcode() %>% # add system via kcode
@@ -123,12 +123,12 @@ vwp_compute_pwp <- function(plays, PWP, coef_matrices){
   vb_skills <- bind_rows(vb_net_attacks, vb_system_attacks, (vb_digs %>% select(-distance)), vb_sets, vb_serves, vb_freeballs) %>% select(id, input_pwp)
 
   # have to think about this later - people delete columns from their dv files   
-  vb_pwp <- left_join(vbID, vb_skills, by = "id") %>% select(id, team, opponent, match_id, point_id, video_time, player_name,
-                                                             skill, skill_type, evaluation_code, evaluation, attack_code, set_code,
-                                                             start_zone, end_zone, end_subzone, skill_subtype, num_players, set_number,
-                                                             #home_p2, home_p3, home_p4, visiting_p2, visiting_p3, visiting_p4,
-                                                             team_touch_id, home_team, visiting_team, point_won_by, add_cover, serving_team, phase,
-                                                             blockers, system, attack_next, attack_start_zone, set_zone, block_touched, attack_type, time_to_touch, input_pwp)
+  vb_pwp <- left_join(vbID, vb_skills, by = "id") #%>% select(id, team, opponent, match_id, point_id, video_time, player_name,
+                                                  #           skill, skill_type, evaluation_code, evaluation, attack_code, set_code,
+                                                  #           start_zone, end_zone, end_subzone, skill_subtype, num_players, set_number,
+                                                  #           #home_p2, home_p3, home_p4, visiting_p2, visiting_p3, visiting_p4,
+                                                  #           team_touch_id, home_team, visiting_team, point_won_by, add_cover, serving_team, phase,
+                                                  #           blockers, system, attack_next, attack_start_zone, set_zone, block_touched, attack_type, time_to_touch, input_pwp)
   
   # Step 5: get output point-win-probability
   vb_pwp2 <- vb_pwp %>% filter(!(skill %in% c("Block", "Reception"))) %>%
